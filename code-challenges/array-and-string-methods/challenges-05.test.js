374c1b7c15d6a731467b8785c44a0338213a8906
Diana Kim <dianajkim@gmail.com>
2019-10-30 16:12:40 -0700

    challenge 5 passed

--- code-challenges/array-and-string-methods/challenges-05.test.js
+++ code-challenges/array-and-string-methods/challenges-05.test.js
@@ -124,6 +124,9 @@ Return a new array containing just the verbs. For example, ['Mix until evenly di
 const stepActions = (recipe) => {
   let result = [];
   // Solution code here...
+  for (let i = 0; i < recipe.steps.length; i++) {
+    result.push((recipe.steps[i].split(' '))[0]);
+  }
   return result;
 };
 
@@ -245,7 +248,7 @@ describe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should return a list of recipe steps', () => {
     expect(stepActions(gruffaloCrumble)).toStrictEqual(['Pre-heat', 'De-prickle', 'Sprinkle', 'Mix', 'Grease', 'Combine', 'Fold', 'Spread', 'Bake']);
     expect(stepActions(gruffaloCrumble).length).toStrictEqual(9);
