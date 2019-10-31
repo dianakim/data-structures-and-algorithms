7043ddc7bcb8027111de19936b9935e7da888d2f
Diana Kim <dianajkim@gmail.com>
2019-10-30 19:20:20 -0700

    challenge 6 passed

--- code-challenges/array-and-string-methods/challenges-05.test.js
+++ code-challenges/array-and-string-methods/challenges-05.test.js
@@ -145,6 +145,13 @@ For example:
 
 const removeEvenValues = (arr) => {
   // Solution code here...
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -255,7 +262,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should remove the even numbers from the array', () => {
     let list = [1, 2, 3, 4, 5, 6];
     removeEvenValues(list);
