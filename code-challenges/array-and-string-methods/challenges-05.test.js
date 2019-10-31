be81cb93cd5673ff5cb074026de9bdbbec81c83e
Diana Kim <dianajkim@gmail.com>
2019-10-30 19:49:02 -0700

    challenge 8 passed

--- code-challenges/array-and-string-methods/challenges-05.test.js
+++ code-challenges/array-and-string-methods/challenges-05.test.js
@@ -191,6 +191,13 @@ Write a function named totalSumCSV that, given a string of comma-separated value
 const totalSumCSV = (str) => {
   let total = 0;
   // Solution code here...
+  let strArr = str.split(',');
+  console.log(strArr);
+
+  strArr.forEach((value) => {
+    total += parseInt(value);
+  });
+
   return total;
 };
 
@@ -299,7 +306,7 @@ describe('Testing challenge 7', () => {
   });
 });
 
-xdescribe('Testing challenge 8', () => {
+describe('Testing challenge 8', () => {
   test('It should add up the numbers contained within the string', () => {
     expect(totalSumCSV('1,4,5,7,2')).toStrictEqual(19);
     expect(totalSumCSV('147')).toStrictEqual(147);
