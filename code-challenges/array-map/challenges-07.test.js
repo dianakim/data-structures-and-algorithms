bc0216fca0c668d9a9d43a4d0a95ab42b60af607
Diana Kim <dianajkim@gmail.com>
2019-11-08 23:14:52 -0800

    challenge 2 passed

--- code-challenges/array-map/challenges-07.test.js
+++ code-challenges/array-map/challenges-07.test.js
@@ -28,6 +28,13 @@ Write a function named forEachTwoToThe that produces the same output as your for
 
 const forEachTwoToThe = (arr) => {
   // Solution code here...
+  let productArr = [];
+
+  arr.forEach( value => {
+    productArr.push(Math.pow(2, value));
+  });
+
+  return productArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -183,7 +190,7 @@ describe('Testing challenge 1', () => {
   });
 });
 
-xdescribe('Testing challenge 2', () => {
+describe('Testing challenge 2', () => {
   test('It should return two raised to the power of the integer', () => {
     expect(forEachTwoToThe([0, 4, 5])).toStrictEqual([1, 16, 32]);
     expect(forEachTwoToThe([0, 4, 5]).length).toStrictEqual(3);
