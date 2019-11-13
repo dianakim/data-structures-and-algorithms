0c7ca5572c87a3f626488ca264d4c8fadad645fa
Diana Kim <dianajkim@gmail.com>
2019-11-13 00:07:33 -0800

    challenge 5 passed

--- code-challenges/array-filter/challenges-08.test.js
+++ code-challenges/array-filter/challenges-08.test.js
@@ -120,7 +120,14 @@ For example, getStatName(snorlaxData.stats, 50) will return ['special-defense', 
 
 const getStatName = (arr, minBaseStat) => {
   // Solution code here...
-  
+  let exceedsMinBaseStat = [];
+
+  arr.filter( (val, idx) => {
+    if (val.baseStat > minBaseStat) {
+      exceedsMinBaseStat.push(val.stat.name);
+    }
+  });
+  return exceedsMinBaseStat;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -256,7 +263,7 @@ describe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should return the name of the stats that exceed that maximum', () => {
     expect(getStatName(snorlaxData.stats, 50)).toStrictEqual([ 'special-defense', 'special-attack' ]);
     expect(getStatName(snorlaxData.stats, 50).length).toStrictEqual(2);
